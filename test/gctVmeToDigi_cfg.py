6aacee18726f03fdda96ad8052036ccd8cdfc4f3
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-08-15 17:02:36 +0000

    fixed

--- test/gctVmeToDigi_cfg.py
+++ test/gctVmeToDigi_cfg.py
@@ -11,11 +11,11 @@ process = cms.Process('GctVmeToDigi')
 
 process.source = cms.Source ( "EmptySource" )
   
-process.maxEvents = cms.untracked.PSet ( input = cms.untracked.int32 ( 10 ) )
+process.maxEvents = cms.untracked.PSet ( input = cms.untracked.int32 ( 3564 ) )
   
 process.gctRaw = cms.OutputModule( "TextToRaw",
 #  filename = cms.untracked.string ( "eventsForEmyr2.txt" ),
-  filename = cms.untracked.string("/home/jbrooke/patternCaptureOrbit_ts__2008_08_15__15h05m43s.txt"),
+  filename = cms.untracked.string("/home/jbrooke/patternCaptureOrbit_ts__2008_08_15__18h57m24s.txt"),
   GctFedId = cms.untracked.int32 ( 745 )
 )
   
@@ -47,15 +47,22 @@ process.load('DQMServices.Core.DQM_cfg')
 
 process.load('DQM.L1TMonitor.L1TGCT_cfi')
 process.l1tgct.disableROOToutput = cms.untracked.bool(False)
-process.l1tgct.outputFile = cms.untracked.string('l1tgct.root')
+process.l1tgct.outputFile = cms.untracked.string('gctDqm.root')
+process.l1tgct.gctCentralJetsSource = cms.InputTag("l1GctHwDigis","cenJets")
+process.l1tgct.gctNonIsoEmSource = cms.InputTag("l1GctHwDigis","nonIsoEm")
+process.l1tgct.gctForwardJetsSource = cms.InputTag("l1GctHwDigis","forJets")
+process.l1tgct.gctIsoEmSource = cms.InputTag("l1GctHwDigis","isoEm")
+process.l1tgct.gctEnergySumsSource = cms.InputTag("l1GctHwDigis","")
+process.l1tgct.gctTauJetsSource = cms.InputTag("l1GctHwDigis","tauJets")
 
 process.load('DQM.L1TMonitor.L1TRCT_cfi')
 process.l1trct.disableROOToutput = cms.untracked.bool(False)
-process.l1trct.outputFile = cms.untracked.string('l1trct.root')
+process.l1trct.outputFile = cms.untracked.string('gctDqm.root')
+process.l1trct.rctSource = cms.InputTag("l1GctHwDigis","")
 
-process.p = cms.Path ( process.gctRaw * process.dumpRaw * process.l1GctHwDigis * process.dumpGctDigis )
+#process.p = cms.Path ( process.gctRaw * process.dumpRaw * process.l1GctHwDigis * process.dumpGctDigis )
 
-process.p = cms.Path ( process.gctRaw * process.dumpRaw * process.l1GctHwDigis * process.l1trct * process.l1tgct )
+process.p = cms.Path ( process.gctRaw * process.l1GctHwDigis * process.l1trct * process.l1tgct )
 
 process.output = cms.OutputModule ( "PoolOutputModule",
   outputCommands = cms.untracked.vstring ( 

4d0e9a698f6f4a93d800c23d632e3c84d4970526
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-08-15 14:07:03 +0000

    add DQM to vme job

--- test/gctVmeToDigi_cfg.py
+++ test/gctVmeToDigi_cfg.py
@@ -14,7 +14,8 @@ process.source = cms.Source ( "EmptySource" )
 process.maxEvents = cms.untracked.PSet ( input = cms.untracked.int32 ( 10 ) )
   
 process.gctRaw = cms.OutputModule( "TextToRaw",
-  filename = cms.untracked.string ( "eventsForEmyr2.txt" ),
+#  filename = cms.untracked.string ( "eventsForEmyr2.txt" ),
+  filename = cms.untracked.string("/home/jbrooke/patternCaptureOrbit_ts__2008_08_15__15h05m43s.txt"),
   GctFedId = cms.untracked.int32 ( 745 )
 )
   
@@ -41,7 +42,20 @@ process.dumpGctDigis.doEm = cms.untracked.bool ( True )
 process.dumpGctDigis.doJets = cms.untracked.bool ( True )
 process.dumpGctDigis.doEnergySums = cms.untracked.bool ( True )
 
+# DQM
+process.load('DQMServices.Core.DQM_cfg')
+
+process.load('DQM.L1TMonitor.L1TGCT_cfi')
+process.l1tgct.disableROOToutput = cms.untracked.bool(False)
+process.l1tgct.outputFile = cms.untracked.string('l1tgct.root')
+
+process.load('DQM.L1TMonitor.L1TRCT_cfi')
+process.l1trct.disableROOToutput = cms.untracked.bool(False)
+process.l1trct.outputFile = cms.untracked.string('l1trct.root')
+
 process.p = cms.Path ( process.gctRaw * process.dumpRaw * process.l1GctHwDigis * process.dumpGctDigis )
+
+process.p = cms.Path ( process.gctRaw * process.dumpRaw * process.l1GctHwDigis * process.l1trct * process.l1tgct )
 
 process.output = cms.OutputModule ( "PoolOutputModule",
   outputCommands = cms.untracked.vstring ( 

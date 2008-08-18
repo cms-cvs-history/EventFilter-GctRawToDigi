ddad87832390c8024c6561320e09520c2ac93295
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-08-18 09:43:33 +0000

    delete out of date config files

--- test/gctVmeToDigi_cfg.py
+++ test/gctVmeToDigi_cfg.py
@@ -42,27 +42,7 @@ process.dumpGctDigis.doEm = cms.untracked.bool ( True )
 process.dumpGctDigis.doJets = cms.untracked.bool ( True )
 process.dumpGctDigis.doEnergySums = cms.untracked.bool ( True )
 
-# DQM
-process.load('DQMServices.Core.DQM_cfg')
-
-process.load('DQM.L1TMonitor.L1TGCT_cfi')
-process.l1tgct.disableROOToutput = cms.untracked.bool(False)
-process.l1tgct.outputFile = cms.untracked.string('gctDqm.root')
-process.l1tgct.gctCentralJetsSource = cms.InputTag("l1GctHwDigis","cenJets")
-process.l1tgct.gctNonIsoEmSource = cms.InputTag("l1GctHwDigis","nonIsoEm")
-process.l1tgct.gctForwardJetsSource = cms.InputTag("l1GctHwDigis","forJets")
-process.l1tgct.gctIsoEmSource = cms.InputTag("l1GctHwDigis","isoEm")
-process.l1tgct.gctEnergySumsSource = cms.InputTag("l1GctHwDigis","")
-process.l1tgct.gctTauJetsSource = cms.InputTag("l1GctHwDigis","tauJets")
-
-process.load('DQM.L1TMonitor.L1TRCT_cfi')
-process.l1trct.disableROOToutput = cms.untracked.bool(False)
-process.l1trct.outputFile = cms.untracked.string('gctDqm.root')
-process.l1trct.rctSource = cms.InputTag("l1GctHwDigis","")
-
-#process.p = cms.Path ( process.gctRaw * process.dumpRaw * process.l1GctHwDigis * process.dumpGctDigis )
-
-process.p = cms.Path ( process.gctRaw * process.l1GctHwDigis * process.l1trct * process.l1tgct )
+process.p = cms.Path ( process.gctRaw * process.dumpRaw * process.l1GctHwDigis * process.dumpGctDigis )
 
 process.output = cms.OutputModule ( "PoolOutputModule",
   outputCommands = cms.untracked.vstring ( 

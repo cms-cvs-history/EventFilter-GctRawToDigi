b3f25cfbe6db9ab049ad4b8b9fd4b7a8086c7912
cmsbuild <>
2009-04-20 16:06:55 +0000

    This commit was manufactured by cvs2git to create tag 'EventFilter- GctRawToDigi-V00-12-04'.
    Sprout from master 2009-04-07 11:00:53 UTC Robert Frazier <sha1-273486ace982b9e0b4eb051eb902f3dfb02026ef@cern.ch> 'Fixing compile prob'
    Cherrypick from master 2009-04-20 16:06:54 UTC Robert Frazier <sha1-273486ace982b9e0b4eb051eb902f3dfb02026ef@cern.ch> 'Added in MHT packing.  Tested: works fine in standalone.':
        plugins/GctDigiToRaw.cc
        src/GctFormatTranslateMCLegacy.cc
        src/GctFormatTranslateMCLegacy.h

--- src/GctFormatTranslateMCLegacy.cc
+++ src/GctFormatTranslateMCLegacy.cc
@@ -12,6 +12,7 @@ using std::cout;
 using std::endl;
 using std::pair;
 using std::make_pair;
+using std::vector;
 
 // INITIALISE STATIC VARIABLES
 GctFormatTranslateMCLegacy::BlockLengthMap GctFormatTranslateMCLegacy::m_blockLength = GctFormatTranslateMCLegacy::BlockLengthMap();
@@ -219,7 +220,8 @@ void GctFormatTranslateMCLegacy::writeGctOutJetBlock(unsigned char * d,
                                                      const L1GctJetCandCollection* forJets,
                                                      const L1GctJetCandCollection* tauJets, 
                                                      const L1GctHFRingEtSumsCollection* hfRingSums,
-                                                     const L1GctHFBitCountsCollection* hfBitCounts)
+                                                     const L1GctHFBitCountsCollection* hfBitCounts,
+                                                     const L1GctHtMissCollection* htMiss)
 {
   // Set up a vector of the collections for easy iteration.
   vector<const L1GctJetCandCollection*> jets(NUM_JET_CATEGORIES);
@@ -241,13 +243,11 @@ void GctFormatTranslateMCLegacy::writeGctOutJetBlock(unsigned char * d,
     if((jetCands->size()-offset) < 4) { LogDebug("GCT") << "Insufficient jet candidates with bx=0!\nAborting packing of GCT Jet Output!"; return; }
   }
   
-  // Now find the offset for the jet counts with bx=0
-  unsigned bx0HfRingSumsOffset;
+  // Now find the collection offsets for the HfRingSums, HfBitCounts, and HtMiss with bx=0
+  unsigned bx0HfRingSumsOffset, bx0HfBitCountsOffset, bx0HtMissOffset;
   if(!findBx0OffsetInCollection(bx0HfRingSumsOffset, hfRingSums)) { LogDebug("GCT") << "No ring sums with bx=0!\nAborting packing of GCT Jet Output!"; return; }
-
-  // Now find the offset for the jet counts with bx=0
-  unsigned bx0HfBitCountsOffset;
   if(!findBx0OffsetInCollection(bx0HfBitCountsOffset, hfBitCounts)) { LogDebug("GCT") << "No bit counts with bx=0!\nAborting packing of GCT Jet Output!"; return; }
+  if(!findBx0OffsetInCollection(bx0HtMissOffset, htMiss)) { LogDebug("GCT") << "No missing Ht with bx=0!\nAborting packing of GCT Jet Output!"; return; }
 
   // Now write the header, as we should now have all requisite data.
   writeRawHeader(d, 0x583, 1);  // ** NOTE can only currenly do 1 timesample! **
@@ -286,7 +286,14 @@ void GctFormatTranslateMCLegacy::writeGctOutJetBlock(unsigned char * d,
   tmp |= hfRingSums->at(bx0HfRingSumsOffset).etSum(2)<<19;
   tmp |= hfRingSums->at(bx0HfRingSumsOffset).etSum(3)<<22;
   p32[0] = tmp;
-  p32[1] = 0x00008000;
+  
+  const L1GctHtMiss& bx0HtMiss = htMiss->at(bx0HtMissOffset);
+  uint32_t htMissRaw = 0x5555c000 |
+                       (bx0HtMiss.overFlow() ? 0x1000 : 0x0000) |
+                       ((bx0HtMiss.et() & 0x7f) << 5) |
+                       ((bx0HtMiss.phi() & 0x1f));
+  
+  p32[1] = htMissRaw;
 }
 
 void GctFormatTranslateMCLegacy::writeRctEmCandBlocks(unsigned char * d, const L1CaloEmCollection * rctEm)
@@ -336,18 +343,18 @@ void GctFormatTranslateMCLegacy::writeRctEmCandBlocks(unsigned char * d, const L
   // Now run the conversion
   for(unsigned c = 0 ; c < 18 ; ++c)
   {
-    srcCardRouting_.EMUtoSFP(emuToSfpData[c].eIsoRank, emuToSfpData[c].eIsoCardId, emuToSfpData[c].eIsoRegionId,
-                             emuToSfpData[c].eNonIsoRank, emuToSfpData[c].eNonIsoCardId, emuToSfpData[c].eNonIsoRegionId,
-                             emuToSfpData[c].mipBits, emuToSfpData[c].qBits, emuToSfpData[c].sfp);
+    srcCardRouting().EMUtoSFP(emuToSfpData[c].eIsoRank, emuToSfpData[c].eIsoCardId, emuToSfpData[c].eIsoRegionId,
+                              emuToSfpData[c].eNonIsoRank, emuToSfpData[c].eNonIsoCardId, emuToSfpData[c].eNonIsoRegionId,
+                              emuToSfpData[c].mipBits, emuToSfpData[c].qBits, emuToSfpData[c].sfp);
   }
   
   // Now pack up the data into the RAW format.
-  RctCrateMap::iterator blockStartCrateIter;
-  for(blockStartCrateIter = rctCrate_.begin() ; blockStartCrateIter != rctCrate_.end() ; ++blockStartCrateIter)
+  BlkToRctCrateMap::iterator blockStartCrateIter;
+  for(blockStartCrateIter = rctEmCrateMap().begin() ; blockStartCrateIter != rctEmCrateMap().end() ; ++blockStartCrateIter)
   {
     unsigned blockId = blockStartCrateIter->first;
     unsigned startCrate = blockStartCrateIter->second;
-    unsigned blockLength_32bit = GctBlockHeaderV2(blockId, 1, 0, 0).length();
+    unsigned blockLength_32bit = blockLengthMap()[blockId];
     
     writeRawHeader(d, blockId, 1);
     d+=4; // move past header.
